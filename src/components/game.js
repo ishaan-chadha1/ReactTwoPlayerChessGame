4d9c4efa7c7f41e67ddfae9f9cbe62729b7bcbf5
ishaan-chadha1 <ishaan.chadha12@gmail.com>
2023-08-15 07:59:14 +0530

    Update game.js

--- src/components/game.js
+++ src/components/game.js
@@ -1,21 +1,18 @@
 import React from "react";
 import Board from './board.js';
 
+import './game.css'; // Importing the CSS file for styling
 
 export default class Game extends React.Component {
   render() {
-
     return (
-      <div>
+      <div className="game-container"> {/* Added a class to center align */}
         <div className="game">
           <div className="game-board">
-            <Board
-            />
+            <Board />
           </div>
         </div>
       </div>
-
-
     );
   }
 }
